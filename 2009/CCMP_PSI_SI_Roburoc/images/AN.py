4c2971e743798d95f06cc7ce6b8dd277d9ae8be7
Xavier Pessoles <xpessoles.ptsi@free.fr>
2022-03-20 16:46:39 +0100

    MAJ

--- 2009/CCMP_PSI_SI_Roburoc/images/AN.py
+++ 2009/CCMP_PSI_SI_Roburoc/images/AN.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math as m
 
 d1 = 76
 d4 = 70
 h1 = 292
 
+mE = 60
+g = 9.8
+D = 25/1000 # m
+d = 10/1000 # m
+
+
+ 
 les_beta = np.linspace(-45,30,100000)
 les_lambda = np.sqrt(np.power(d1-d4*np.cos(np.radians(les_beta)),2)+np.power(h1-d4*np.sin(np.radians(les_beta)),2))
 
@@ -13,3 +21,6 @@ plt.xlabel("$\\beta$ (deg)")
 plt.ylabel("$\\lambda$ (mm)")
 plt.grid()
 plt.show()
+
+DeltaPMax = 16.8*mE*g/(m.pi*(D*D-d*d))
+print(DeltaPMax*10e-6)
