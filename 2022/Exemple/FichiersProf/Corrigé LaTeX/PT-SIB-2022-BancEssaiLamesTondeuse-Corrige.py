9590e2d2b838ba1ad5c03dc02065b7b2471f684f
Xavier Pessoles <5823039+xpessoles@users.noreply.github.com>
2022-06-22 11:41:34 +0100

    MAJ

--- 2022/Exemple/FichiersProf/Corrigé LaTeX/PT-SIB-2022-BancEssaiLamesTondeuse-Corrige.py
+++ 2022/Exemple/FichiersProf/Corrigé LaTeX/PT-SIB-2022-BancEssaiLamesTondeuse-Corrige.py
@@ -2,10 +2,17 @@
 # Q3
 Fv = 30000 #N
 mu  = 0.15
-Di = 32.5 #mm
+Di = 32.2 #mm
 De = 60 #mm
 
 Cadh = 1/3*mu *Fv * (De**3-Di**3)/(De**2-Di**2)
 print("Cadh ",Cadh/1000, " Nm")
-e = 1.5
-b = 5
+
+
+e = 1.5  # mm
+b = 4    # mm
+de = 42  # mm
+Rm = 300 # MPa = N/mm²
+
+C_cis = 2*Rm*e*b*de
+print("Ccis ",C_cis/1000, " Nm")
