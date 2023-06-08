d5ff260ecce775a102d114801e611557f67a0789
Xavier Pessoles <xpessoles.ptsi@free.fr>
2023-06-08 21:53:17 +0200

    Update XENS_PSI_InformatiqueB.py

--- 2023/XENS/XENS_PSI_InformatiqueB/images/XENS_PSI_InformatiqueB.py
+++ 2023/XENS/XENS_PSI_InformatiqueB/images/XENS_PSI_InformatiqueB.py
@@ -69,8 +69,97 @@ def différentiel(texte1, texte2) :
 
 
     return diff
+
+
+
+def tranche(arg_début, arg_avant, arg_après):
+    return {'début': arg_début, 'avant': arg_avant, 'après': arg_après}
+
+def début(tr):
+    return tr['début']
+
+def après(tr):
+    return tr['après']
+
+def avant(tr):
+    return tr['avant']
+
+def fin(tr):
+    return début(tr) + len(après(tr))
+
+def applique(texte1,diff) :
+    # Attention, un texte est une liste de chaînes de caractères
+    texte2 = texte1[:]
+    for slice in diff :
+        for i in range(début(slice),fin(slice)):
+            texte2[i]=texte1[i]
+    return texte2
+
+def différentiel(texte1, texte2):
+    diff = []
+    modif = False
+    for i in range(len(texte1)):
+        if texte1[i] != texte2[i] and not modif:
+            modif = True
+            début = i
+            avant = [texte1[i]]
+            après = [texte2[i]]
+        elif texte1[i] != texte2[i] and modif:
+            avant.append(texte1[i])
+            après.append(texte2[i])
+        elif texte1[i] == texte2[i] and modif:
+            modif = False
+            diff.append(tranche(début, avant, après))
+    if modif:
+        diff.append(tranche(début, avant, après))
+    return diff
+
 texte1 = "Le grand château fort."
 texte1 = [lettre for lettre in texte1]
 texte2 = "Le petit chien a soif."
 texte2 = [lettre for lettre in texte2]
-di,d = différentiel(texte1,texte2)
+diff = différentiel(texte1,texte2)
+
+def inverse(diff):
+    inv = []
+    for slice in diff :
+        inv.append(tranche(début(slice),après(slice),avant(slice)))
+    return inv
+######
+def versionne(texte):
+    return {'courant' : texte, 'historique' : [] }
+
+def courant(texte_versionné):
+    return texte_versionné['courant']
+
+def remplace_courant(texte_versionné, texte):
+    texte_versionné['courant'] = texte
+
+def historique(texte_versionné):
+    return texte_versionné['historique']
+######
+
+
+def modifie(texte_versionné,texte):
+    # On récupère les 2 textes
+    texte1 = texte
+    texte2 = courant(texte_versionné)
+    # On détermine le diff
+    diff = différentiel(texte1,texte2)
+
+    # On remplacé le texte courant :
+    remplace_courant(texte_versionné, texte)
+
+    # On modifie l'historique
+    historique(texte_versionné).append(diff)
+
+def annule(texte_versionné):
+    # On récupère le dernier diff et on l'inverse
+    diff = texte_versionné[historique].pop()
+    diff = inverse(diff)
+    # On applique le diff
+    texte2 = applique(texte_versionné['courant'],diff)
+    # On remplace le texte
+    remplace_courant(texte_versionné, texte2):
+
+
